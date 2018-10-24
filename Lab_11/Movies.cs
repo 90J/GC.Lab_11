8b082f8f30b8176a9b7667704942b09a2fd2c53f
Jay Stiles <JayStiles@quickenloans.com>
2018-10-24 09:49:03 -0400

    submit

---
+++ Lab_11/Movies.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class TheMovieList
+    {
+        public TheMovieList(string userInput, string v)
+        {
+            ArrayList movieList = new ArrayList();
+            {
+                movieList.Add(new TheMovieList("scifi", "Sharktopus"));
+                movieList.Add(new TheMovieList("animated", "Shrek"));
+                movieList.Add(new TheMovieList("drama", "Dead Poets Society"));
+                movieList.Add(new TheMovieList("horror", "The Notebook"));
+                movieList.Add(new TheMovieList("scifi", "Sharknado"));
+                movieList.Add(new TheMovieList("animated", "Aladin"));
+                movieList.Add(new TheMovieList("drama", "St. Elmo's Fire"));
+                movieList.Add(new TheMovieList("horror", "Killer Clowns from Space"));
+                movieList.Add(new TheMovieList("scifi", "Lavalanchula"));
+                movieList.Add(new TheMovieList("animated", "Toy Story"));
+            };
+
+            //get movies from list
+            
+
+        }
+
+    }
+}
---
+++ Lab_11/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        ////Get user input
+        Start:
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine();
+            Console.WriteLine("There are 10 movies in this list.");
+            Console.WriteLine("Please choose a category.");
+            Console.WriteLine("(1) SciFi");
+            Console.WriteLine("(2) Animated");
+            Console.WriteLine("(3) Drama");
+            Console.WriteLine("(4) Horror");
+            Console.Write("What category are you interested in? ");
+            string userInput = (Console.ReadLine());
+
+
+            
+
+            //Calls from here
+            
+
+            //Output
+            
+            Console.WriteLine("Continue? (y/n): ");
+            string keepGoing = Console.ReadLine();
+            if (keepGoing != "y")
+            {
+                Console.WriteLine("Goodbye!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                goto Start;
+            }
+        }
+    }
+}
